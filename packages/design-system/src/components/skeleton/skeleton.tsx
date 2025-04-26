cead328bf3e7677a4f8d9193b09e960190aa7d66
Thiago Mota <thiagomotasilva2000@gmail.com>
2025-04-26 17:07:18 -0300

    feat(skeleton): add Skeleton component

---
+++ packages/design-system/src/components/skeleton/skeleton.tsx
@@ -0,0 +1,11 @@
+import { cn } from "@fiap-tech-challenge/design-system/lib/utils"
+
+export function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="skeleton"
+      className={cn("bg-accent animate-pulse rounded-md", className)}
+      {...props}
+    />
+  )
+}
