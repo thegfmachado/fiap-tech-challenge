df8baa0022fa0eea0e8a9d51b194de5351585738
Thiago Mota <thiagomotasilva2000@gmail.com>
2025-04-26 17:50:05 -0300

    feat(toaster): refactor Toaster component and add useToaster hook

--- packages/design-system/src/components/toaster/toaster.tsx
+++ packages/design-system/src/components/toaster/toaster.tsx
@@ -1,24 +1,14 @@
 "use client"
 
-import { useTheme } from "next-themes"
 import { Toaster as Sonner, ToasterProps } from "sonner"
 
-export function Toaster({ ...props }: ToasterProps) {
-  const { theme = "system" } = useTheme()
+import { useToaster } from "./use-toaster";
+
+export function Toaster(props: ToasterProps) {
+  const properties = useToaster(props);
 
   return (
-    <Sonner
-      theme={theme as ToasterProps["theme"]}
-      className="toaster group"
-      style={
-        {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
-        } as React.CSSProperties
-      }
-      {...props}
-    />
+    <Sonner {...properties} />
   )
 }
 
